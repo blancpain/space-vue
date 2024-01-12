57e7879ea9aa4c22e9d834ce7763ed21b8d13475
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-01-12 12:44:24 +0200

    chore: set up primevue UI lib; css cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
-import './assets/main.css'
-
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/vela-purple/theme.css'
+import 'primeicons/primeicons.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// UI components
+import Button from 'primevue/button'
+import Menubar from 'primevue/menubar'
+
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue)
+app.component('PrimeButton', Button)
+app.component('MenuBar', Menubar)
 
 app.mount('#app')
