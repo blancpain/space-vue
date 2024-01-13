3263e4dfd5c18f2828414b5c7be8b6461e5d08c0
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-01-13 17:25:35 +0200

    feat: add skeletons for login and dashboard pages

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores';
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <main>
+    <div v-for="user in authStore.users" :key="user.id">{{ user.name }} {{ user.email }}</div>
+  </main>
+</template>
