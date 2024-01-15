21497e6153b0bed86807837a973cfcc458daf30e
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-01-15 18:50:39 +0200

    chore: finalise satelite view

--- src/views/SateliteView.vue
+++ src/views/SateliteView.vue
@@ -11,31 +11,40 @@ watch(coords, () => {
     coordsAvailable.value = true;
   }
 });
-
-// TODO: just add some styling and a title...
 </script>
 
 <template>
   <main>
-    <div v-if="coordsAvailable">
-      <GMapMap
-        :center="{ lat: coords.latitude, lng: coords.longitude }"
-        :zoom="10"
-        map-type-id="hybrid"
-        style="width: 100vw; height: 20rem"
-        :options="{
-          zoomControl: true,
-          mapTypeControl: true,
-          scaleControl: true,
-          streetViewControl: true,
-          rotateControl: true,
-          fullscreenControl: true
-        }"
-      />
-    </div>
+    <h1>Check out the satelite view below</h1>
+    <GMapMap
+      v-if="coordsAvailable"
+      :center="{ lat: coords.latitude, lng: coords.longitude }"
+      :zoom="10"
+      map-type-id="hybrid"
+      style="height: 30rem; max-width: 700px"
+      :options="{
+        zoomControl: true,
+        mapTypeControl: true,
+        scaleControl: true,
+        streetViewControl: true,
+        rotateControl: true,
+        fullscreenControl: true
+      }"
+    />
     <div v-else-if="!coordsAvailable">
       <LoadingSpinner />
     </div>
     <div v-else>Something went wrong...</div>
   </main>
 </template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin-bottom: 2rem;
+}
+.vue-map-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
